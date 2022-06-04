9f5972817816601c7dbef0e29e32db8c4f7b8045
Michiel Borkent <michielborkent@gmail.com>
2022-06-04 12:30:14 +0200

    Fix merge

--- src/babashka/cli/exec.clj
+++ src/babashka/cli/exec.clj
@@ -4,8 +4,20 @@
    [clojure.edn :as edn]
    [clojure.string :as str]))
 
-(defn merge-opts [m & ms]
-  (reduce #(merge-with merge %1 %2) m ms))
+(defn- into-able? [x]
+  (and (coll? x)
+       (or (sequential? x)
+           (set? x))))
+
+(defn- merge* [x y]
+  (cond (and (map? x) (map? y)) (merge x y)
+        (and (into-able? x)
+             (into-able? y))
+        (into x y)
+        :else y))
+
+(defn- merge-opts [m & ms]
+  (reduce #(merge-with merge* %1 %2) m ms))
 
 (defn -main
   "Main entrypoint for command line usage.
