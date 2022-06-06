c330fc1f5e9a40a364cc699a7ac33a240693e968
Michiel Borkent <michielborkent@gmail.com>
2022-06-06 16:54:15 +0200

    tests

--- test/babashka/test_runner.clj
+++ test/babashka/test_runner.clj
@@ -7,10 +7,37 @@
                                :excludes :keywords
                                :only :symbols}}}
   (:refer-clojure :exclude [test])
-  (:require [cognitect.test-runner.api :as api]))
+  (:require
+   [clojure.test :as test]
+   [cognitect.test-runner.api :as api]))
+
+(def fail-meth (get-method test/report :fail))
+(def err-meth (get-method test/report :error))
+
+(def test-var (atom {}))
+
+(def cmd (atom nil))
+
+(defn print-only []
+  (println)
+  (println (str @cmd) ":only" (let [v (:var @test-var)
+                                    v (meta v)]
+                                (symbol (str (ns-name (:ns v))) (str (:name v))))))
+
+(defmethod test/report :fail [m]
+  (print-only)
+  (fail-meth m))
+
+(defmethod test/report :error [m]
+  (print-only)
+  (err-meth m))
+
+(defmethod test/report :begin-test-var [m]
+  (reset! test-var m))
 
 (defn test [opts]
-  (let [only (:only opts)
+  (let [_ (reset! cmd (:cmd opts))
+        only (:only opts)
         opts
         (if only
           (if (qualified-symbol? only)
