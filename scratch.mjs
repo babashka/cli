ee4f5a7583310e6eb512aa9d877e7f56775c6eeb
Michiel Borkent <michielborkent@gmail.com>
2023-11-02 22:19:16 +0100

    wip

--- scratch.mjs
+++ scratch.mjs
@@ -2,3 +2,5 @@ import { deepEqual } from 'assert';
 import { parse_args } from './js/babashka/cli.mjs';
 
 console.log(parse_args(['-a', '1', '-a', '1'], {coerce: {a: ['long']}}))
+
+console.log(parse_args(["--no-option", "--no-this-exists"], ({ "coerce": ({ "no-this-exists": "bool" }) })));
